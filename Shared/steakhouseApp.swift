7ec394d6607c93acca92644356e7caf2fc699e87
ilyapasyuk <pasyuk.com@gmail.com>
2021-06-13 11:20:32 +0300

    Initial Commit

---
+++ Shared/steakhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  steakhouseApp.swift
+//  Shared
+//
+//  Created by Ilya Pasyuk on 13.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct steakhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
